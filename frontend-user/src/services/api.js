0f052c87cf31a16a54c23017bc207d5cf0e330a3
Dinesh Kumar <dineshsanjay2009@gmail.com>
2026-04-01 10:59:52 +0530

    update api url

--- frontend-user/src/services/api.js
+++ frontend-user/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://service-backend-nuxs.onrender.com/api",
+  baseURL: "https://servicehub-backend-myaj.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
